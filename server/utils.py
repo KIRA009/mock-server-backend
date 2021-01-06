2ad241d1f80ad747adc69fabcf2cc2ce67a0dc59
Shreyas K <shreyaskumar1035@gmail.com>
2021-01-06 23:28:09 +0530

    [feat] Add pagination of results
    Added following
    - Support pagination of results
    
    Changed following:
    - RelativeEndpoint meta_data num_pages is now num_records

--- server/utils.py
+++ server/utils.py
@@ -1,20 +1,54 @@
 from app.models import Schema
 
 from .fakers import get_random_value
+import json
 
 class Response:
-    def __init__(self, fields):
+    def __init__(self, fields, meta_data, page_no):
         self.fields = fields
+        self.meta_data = json.loads(meta_data)
+        # NOTE: hardcoded for testing
+        self.meta_data['records_per_page'] = 5
+        self.meta_data['num_records'] = 13
+        self.page_no = int(page_no)
         self.cache = dict()  # maybe used for caching results
         self.mapping = dict(
             value=self._value_field,
             schema=self._schema_field
         )
 
+    def get_data(self, count):
+        data = list()
+
+        for _ in range(count):
+            page = dict()
+            for field in self.fields:
+                page[field.key] = self.mapping[field.type](field)
+            data.append(page)
+
+        return data
+
     def create_response(self):
         response = dict()
-        for field in self.fields:
-            response[field.key] = self.mapping[field.type](field)
+        response['items'] = list()
+
+        if self.meta_data['is_paginated']:
+            response['page_no'] = self.page_no
+            response['total_pages'] = (int)(self.meta_data['num_records'] / self.meta_data['records_per_page'])
+            
+            if (self.meta_data['num_records'] % self.meta_data['records_per_page']) != 0:
+                response['total_pages'] += 1
+            
+            if self.page_no > response['total_pages'] or self.page_no < 1:
+                return {}
+
+            response['items'] = \
+                    self.get_data( \
+                        min(self.meta_data['num_records'] - (self.page_no - 1) * self.meta_data['records_per_page'], \
+                        self.meta_data['records_per_page']) \
+                    )
+        else:
+            response['items'] = self.get_data(self.meta_data['num_records'])
 
         return response
 
